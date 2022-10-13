5c54ee9c0ec06696a1bdb311882f600e3691f4ad
Alasdair Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-10-13 09:36:16 +0100

    Update format_primarycare_records.R

--- code/format_primarycare_records.R
+++ code/format_primarycare_records.R
@@ -7,8 +7,56 @@ datapath <- "/Volumes/EHR Group/GPRD_GOLD/Ali/2022_biobank/"
 dir.create(here::here("data", "temp_store"), showWarnings = FALSE)
 system(paste0("wget -nd https://biota.ndph.ox.ac.uk/tabserv.cgi?x=180721904008002d00985d6890ea037fWzQmaSN0PTE2NjU1NzgxMDUmcyNkPWdwX2NsaW5pY2FsJmkjYT03NDMxMSZpI3I9MTIyNTQzXQ== -P ", here::here("data", "temp_store", "gp_clinical.txt"), " -Ogp_clinical.txt"))
 
-clin_test <- read_delim(here::here("temp_store/gp_clinical_test.txt"), delim = "\t")
+gp_clinical <- read_delim(here::here("temp_store/ukb.txt"), delim = "\t")
   
+# look for eczema codes in tpp --------------------------------------------
+gp_clinical_tpp <- gp_clinical %>% 
+  filter(data_provider == 3)
+gp_clinical_tpp %>% 
+  filter(str_detect(read_3, "M111"))
+eczema_tpp <- gp_clinical_tpp %>% 
+  inner_join(eczema_mapped, by = c("read_3" = "readv3_code"))
+eczema_tpp %>% View()
+eczema_primarycare <- eczema_tpp$eid
+
+# look for eczema codes in read v2 ----------------------------------------
+gp_clinical_emisvision <- gp_clinical %>% 
+  filter(data_provider != 3)
+gp_clinical_emisvision %>% 
+  filter(str_detect(read_2, "M111."))
+eczema_mapped_readv2 <- eczema_mapped %>% 
+  filter(match_mark == 1) %>% 
+  group_by(readv2_code) %>% 
+  slice(1) %>% 
+  ungroup()
+eczema_emisvision <- gp_clinical_emisvision %>% 
+  inner_join(eczema_mapped_readv2, by = c("read_2" = "readv2_code"))
+  
+
+# combine TPP and EMIS/VISION ---------------------------------------------
+eczema_primary_care <- eczema_tpp %>% 
+  bind_rows(eczema_emisvision) %>% 
+  group_by(eid) %>% 
+  slice(1) %>% 
+  ungroup() %>% 
+  dplyr::select(f.eid = eid, data_provider, event_dt, read_2, read_3, termv3_desc) %>% 
+  mutate(eczema_gp = 1)
+
+# compare to UKB ----------------------------------------------------------
+included_ids <- gp_clinical$eid %>% unique()
+eczema_test <- eczema %>% 
+  filter(f.eid %in% included_ids) %>% 
+  full_join(eczema_primary_care, by = "f.eid")
+mismatch <- ukb_descriptive %>% 
+  filter(f.eid %in% included_ids) %>% 
+  mutate(dob = as.Date(paste(year(study_entry)-age_at_recruit, "07", "01", sep = "-"))) %>% 
+  left_join(eczema_test, by = "f.eid") %>% 
+  filter(eczema == 1 | !is.na(eczema_gp)) %>% 
+  mutate(age_eczema_gp = as.numeric((event_dt - dob)/365.25))
+
+ggplot(mismatch, aes(x = age_at_recruit , y = age_eczema_gp)) +
+  geom_point()
+
 # gp_clinical <- read_table(paste0(datapath, "gp_clinical.txt"))
 # saveRDS(gp_clinical, paste0(datapath, "gp_clinical.rds"))
 
