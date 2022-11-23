e5a0194a9e2de8a1d2f7ea0ae07ecac8c346f461
julianmatthewman <48559292+julianmatthewman@users.noreply.github.com>
2022-11-23 10:36:12 +0000

    Update combine.R

--- codelist/Eczema Rx from ALSPAC algorithms study/combine.R
+++ codelist/Eczema Rx from ALSPAC algorithms study/combine.R
@@ -1,1 +1,8 @@
-list.files("codelists/Eczema Rx from ALSPAC algorithms study")
+eczemaRx_julian <- list.files("codelist/Eczema Rx from ALSPAC algorithms study", 
+                              pattern = "codelist", full.names = TRUE) %>% 
+  map_dfr(read_csv)
+
+
+#Run 2_
+
+data_match <- gp_script[drug_text %in% eczRx_mapped$cprd_name_search]
