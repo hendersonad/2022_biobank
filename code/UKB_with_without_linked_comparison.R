4bda1f1db86c3ea1ee2f97775d039f821b26e81a
julianmatthewman <48559292+julianmatthewman@users.noreply.github.com>
2023-02-22 17:59:29 +0000

    Update UKB_with_without_linked_comparison.R

--- code/UKB_with_without_linked_comparison.R
+++ code/UKB_with_without_linked_comparison.R
@@ -13,6 +13,34 @@ a <- baseline_assessment_cohort %>%
   tbl_summary()
 
 b <- baseline_assessment_cohort %>% 
+  filter(!f.eid %in% ukb_gp_combined$f.eid) %>% 
+  select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
+  tbl_summary()
+
+c <- ukb_gp_combined %>% 
+  filter(f.eid %in% ukb_gp_combined$f.eid) %>% 
+  select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
+  tbl_summary()
+
+d <- ukb_gp_combined %>% 
+  filter(is.na(phq9_interest)) %>% 
+  select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
+  tbl_summary()
+
+e <- ukb_gp_combined %>% 
+  filter(!is.na(phq9_interest)) %>% 
+  select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
+  tbl_summary()
+
+merged <- tbl_merge(list(a,b,c,d,e))
+merged
+
+# Mental health survey respondents vs non-respondents
+a <- baseline_assessment_cohort %>% 
+  select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
+  tbl_summary()
+
+b <- baseline_assessment_cohort %>% 
   filter(f.eid %in% ukb_gp_combined$f.eid) %>% 
   select(sex, ethnicity, emplyment, age_at_recruit, deprivation, year_entry, phq9_interest, gad7_irritability, eczema, psoriasis, anxiety, depression) %>% 
   tbl_summary()
